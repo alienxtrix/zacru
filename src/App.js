fff5c2694bdcb4179cd24ce923523b3f7459d7b0
Josh Wolf <alienxtrix@gmail.com>
2020-03-17 22:34:43 -0600

    zacru

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './z2.png';
+import fb from './fb.svg';
+import tw from './tw.svg';
+import ins from './ins.svg';
+import ml from './ml.png';
+
 import './App.css';
 
 function App() {
@@ -7,37 +12,44 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          En contrucción<code></code>
+        <p margin="10px">
+          En construcción
         </p>
         <a
           className="App-link"
+        >
+          Contactános </a>
+         <div className="Flex-socialmedia">
+          <a className="App-sm"
           href="https://www.facebook.com/ZACRUTM/"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contactanos en facebook
+          rel="noopener noreferrer"><img src={fb} className="App-socialmedia" alt="logo" /></a> 
+          <a  className="App-sm"
+          href="https://twitter.com/ZacruTM/"
+          target="_blank"
+          rel="noopener noreferrer"><img src={tw} className="App-socialmedia" alt="logo" />
+          </a>
+          <a className="App-sm"
+           href="https://www.instagram.com/zacrutm/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src={ins} className="App-socialmedia" alt="ins" />
         </a>
+          </div>
+     
         <a
           className="App-link"
-          href="https://www.twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Contactanos en twitter
-        </a>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/"
+          Visualiza nuestros productos en</a>
+          <a href="https://listado.mercadolibre.com.mx/_CustId_417292318"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contactanos en instagram
+          rel="noopener noreferrer">
+          <img src={ml} alt="ml" padding="5px" />
         </a>
         
       </header>
       <footer className="App-footer">
-  <p>Posted by: Kevin Cruz & Josh Fuentes </p>
+  <p>Posted by: Kevs, Josh & Dan</p>
   <p>Contact information: <a className="App-link" href="mailto:someone@example.com">info@zacru.com.mx</a>.</p>
 <a>© 2020 zacru</a>
 </footer>
